90b72cf58c02e149aa8d6f1ea9f6114bae9bf8d5
pebos <66707540+pebos@users.noreply.github.com>
2024-08-25 06:44:55 +0000

    Add files via upload

---
+++ get-info.ps1
@@ -0,0 +1,25 @@
+﻿$timestamp = Get-Date -Format "yyyy-MM-dd_hh-mm"
+do {
+    $driveLetter = Read-Host "Enter the drive letter where you want to save the outputs (e.g., 'E:')"
+    $driveExists = Test-Path -Path "${driveLetter}:\"
+    if (-not $driveExists) {
+        Write-Host "Specified drive $driveLetter does not exist. Please enter a valid drive letter."
+    }
+} until ($driveExists)
+
+# Grabs DNS cache and saves it to a file
+Get-DnsClientCache > "${driveLetter}:\$($timestamp)_DNS_Cache.txt"
+
+#Grabs WiFi passwords and saves it to a file
+    $wifiProfiles = netsh wlan show profiles | 
+     Select-String ":(.+)$" | 
+     ForEach-Object {
+         $name = $_.Matches.Groups[1].Value.Trim()
+         netsh wlan show profile name="$name" key=clear
+      } | 
+      Select-String "Key Content\W+\:(.+)$" | 
+      ForEach-Object {
+          $password = $_.Matches.Groups[1].Value.Trim()
+          [PSCustomObject]@{ SSID = $name; PASSWORD = $password }
+      }
+$wifiProfiles | Format-Table -AutoSize | Out-File "${driveLetter}:\${timestamp}_WiFi_Passwords.txt"
